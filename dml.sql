4a3b28d5dda058b3051181aebe8c15dc8e242b3f
xjhxcc <45287894+xjhxcc@users.noreply.github.com>
2019-07-02 20:37:10 +0800

    dml.sql作业答案

--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
 -- 插入记录  
-
+insert into student values(003,'王五',19,'男');
 -- 修改记录  
-
+update student set name='王五五' where id=003;
 --  删除记录  
-
+delete from student where id=003;
 -- 查询记录  
+select * from student;
+select * from student where name='张三';
